341db22927a2f4c246f28b3b81fd5151f0de83ce
Sirius Black <mariia.beliaev@gmail.com>
2023-04-12 00:46:49 +0300

    I hate math :( added iteration solution

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 // Итерационный подсчет n-го числа Фибоначчи
-
-
-
+const fibonacciIter = (n) => {
+  let firstNumber = 0;
+  let secondNumber = 1;
+  let nthNumber;
+  for (let i = 2; i <= n; i++) {
+    nthNumber = firstNumber + secondNumber;
+    firstNumber = secondNumber;
+    secondNumber = nthNumber;
+  }
+  return nthNumber;
+};
+console.log(fibonacciIter(7));
 
 // Рекурсивное вычисление n-го числа Фибоначчи
-
-
-
+const fibonacciRec = () => {};
